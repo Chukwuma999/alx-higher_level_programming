fa051be2d2df4f1a7ced5ff67d0fc61ccb935757
Chukwumaobi Precious Nguma <99540489+Chukwuma999@users.noreply.github.com>
2022-06-08 22:39:12 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -13,4 +13,22 @@ if __name__ == '__main__':
 
     new_matrix = square_matrix_simple(matrix)
     print(new_matrix)
-    print(matrix)
+    print(matrix)#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    if matrix is not None:
+
+        new = []
+
+        for rows in matrix:
+
+            new.append(list(map(lambda x: x**2, rows)))
+
+        return (new)
+
+    return None
+
+# return[[elem**2 in row] for row in matrix]
+
+# return(list(map(lambda x: x**2, list)) for list in matrix)
