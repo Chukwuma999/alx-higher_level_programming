ea4a282314944fed8083fc802d413c1a6b1371f3
Chukwuma999 <chukwumaobinguma@gmail.com>
2022-07-05 18:55:07 +0200

    Fixing code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module 3-write_file
+Module 1-write_file
 
 Contains function that writes to text file and returns num chars written
 """
@@ -10,4 +10,3 @@ def write_file(filename="", text=""):
     """writes to text file and returns num chars written"""
     with open(filename, mode="w", encoding="utf-8") as f:
         return(f.write(text))
-        
